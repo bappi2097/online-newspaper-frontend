571eb27e46de36beda9e68dde67b9fbb9513da2b
bappi2097 <bappi35-2097@diu.edu.bd>
2021-01-15 20:52:20 +0600

    grid added

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,17 @@
+window.onscroll = function() {myFunction()};
+
+var navbar = document.querySelector("nav");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky");
+        navbar.classList.add("top-0");
+  } else {
+        navbar.classList.remove("sticky");
+        navbar.classList.remove("top-0");
+    }
+}
 const event = new Date();
 const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 document.querySelector("#date").innerText=event.toLocaleDateString('bn-BD', options);
